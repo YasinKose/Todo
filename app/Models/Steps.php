d3740765d1490c0e709bb760271d13c2c83b1b40
YasinKose <yasin.kose.123@gmail.com>
2021-06-12 14:28:09 +0300

    Added jobs into the steps model

--- app/Models/Steps.php
+++ app/Models/Steps.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Steps extends Model
 {
     protected $fillable = ['name', 'status', 'user_id'];
+
+    /**
+     * @return BelongsTo
+     */
+    public function jobs(): BelongsTo
+    {
+        return $this->belongsTo(Jobs::class);
+    }
 }
